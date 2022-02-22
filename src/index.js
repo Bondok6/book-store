2c2e51ac326aebe81565f6b7e11088852edab265
Bondok6 <kirolousbondok6@gmail.com>
2022-02-22 22:48:09 +0200

    Fix sylelint error by Adding index.css file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import App from './App';
 
 ReactDOM.render(
