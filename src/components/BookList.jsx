28b78d9ef5216922a0c659fba2acf5519a6542c2
Bondok6 <kirolousbondok6@gmail.com>
2022-02-22 19:46:45 +0200

    Remove DUMMYDATA

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,33 +3,12 @@ import { useSelector } from 'react-redux';
 import BookItem from './BookItem';
 
 const BookList = () => {
-  const DUMMYDATA = [
-    {
-      id: '1',
-      title: 'The Hunger Game',
-      author: 'Suzanne Collins',
-      category: 'action',
-    },
-    {
-      id: '2',
-      title: 'Dune',
-      author: 'Frank Herbert',
-      category: 'science fiction',
-    },
-    {
-      id: '3',
-      title: 'Capital in the Twenty-First Century',
-      author: 'Suzanne Collins',
-      category: 'economy',
-    },
-  ];
-
   const books = useSelector((state) => state.booksReducer);
 
   return (
     <div>
       <ul>
-        {[...DUMMYDATA, ...books].map((book) => (
+        {books.map((book) => (
           <BookItem
             key={book.id}
             id={book.id}
